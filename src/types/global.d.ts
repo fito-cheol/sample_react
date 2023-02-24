334c8004970ef3f492fdae3275479afb27dc1e44
김철 <dmsghs92@gmail.com>
2023-02-25 00:22:16 +0900

    Add Global.ts

---
+++ src/types/global.d.ts
@@ -0,0 +1,32 @@
+// https://bobbyhadz.com/blog/typescript-make-types-global
+// To declare global types in TypeScript:
+
+// Create a global.d.ts file and declare types in the global namespace.
+// Add types or interfaces that need to be globally accessible.
+// Make the file a module by using export {}.
+export {};
+declare global {
+  interface Coordinate {
+    x: number;
+    y: number;
+  }
+
+  interface Shape {
+    name: string;
+    coordinates: Coordinate[];
+    width: number;
+    height: number;
+  }
+
+  interface Tile {
+    shapeName: string;
+    color: string;
+    position: Coordinate;
+  }
+
+  interface Board {
+    width: number;
+    height: number;
+    tiles: Tile[];
+  }
+}
