9296bc57ab2f390ccdabc74933f8cf413dcd60e7
LAPTOP-TQFM3LTV\dmsgh <dmsghs92@gmail.com>
2025-01-29 18:26:41 +0900

    feat: Add Eslint with Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,57 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+import prettierPlugin from 'eslint-plugin-prettier';
+import unusedImports from 'eslint-plugin-unused-imports';
 
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-  { ignores: ['dist'] },
-  {
-    files: ['**/*.{js,jsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
-    settings: { react: { version: '18.3' } },
-    plugins: {
-      react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-      ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-]
+	pluginJs.configs.recommended,
+	pluginReact.configs.flat.recommended,
+	{
+		files: ['**/*.{js,mjs,cjs,jsx,ts,tsx}'],
+		languageOptions: {
+			ecmaVersion: 2022,
+			sourceType: 'module',
+			globals: {
+				...globals.browser,
+				...globals.node,
+				...globals.es2022,
+			},
+			parserOptions: {
+				ecmaFeatures: {
+					jsx: true,
+				},
+				ecmaVersion: 'latest',
+				sourceType: 'module',
+			},
+		},
+		plugins: {
+			react: pluginReact,
+			'react-hooks': pluginReact,
+			prettier: prettierPlugin,
+			'unused-imports': unusedImports,
+		},
+		rules: {
+			'react/react-in-jsx-scope': 'off',
+			'react/prefer-stateless-function': 'off',
+			'react/jsx-filename-extension': 'off',
+			'react/jsx-one-expression-per-line': 'off',
+			'no-nested-ternary': 'off',
+			// Disable the default no-unused-vars rule
+			'no-unused-vars': 'off',
+			// Enable the plugin's rule for unused variables and imports
+			'unused-imports/no-unused-imports': 'error',
+			'unused-imports/no-unused-vars': [
+				'warn',
+				{
+					vars: 'all',
+					varsIgnorePattern: '^_', // Ignore variables prefixed with _
+					args: 'after-used',
+					argsIgnorePattern: '^_', // Ignore arguments prefixed with _
+				},
+			],
+			...prettierPlugin.configs.recommended.rules,
+		},
+	},
+];
