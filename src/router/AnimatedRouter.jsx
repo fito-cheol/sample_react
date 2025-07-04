710a8db89419e1ef75066e40069462724036613a
LAPTOP-TQFM3LTV\dmsgh <dmsghs92@gmail.com>
2025-07-04 11:23:04 +0900

    fix: 화면전환 로직 수정

--- src/router/AnimatedRouter.jsx
+++ src/router/AnimatedRouter.jsx
@@ -10,12 +10,17 @@ import '@/assets/style/components/transition.scss';
 function AnimatedRouter() {
 	const location = useLocation();
 	const navigationType = useNavigationType();
-	const transitionClass = navigationType === 'POP' ? 'Backward' : 'Forward';
-	const [prevLocation, setPrevLocation] = useState(location);
-	const [transitioning, setTransitioning] = useState(false);
+	const transitionClass = navigationType === 'POP' ? 'backward' : 'forward';
+
+	const [rerenderTrigger, setRerenderTrigger] = useState(true);
+
 	const mainRef = useRef('A');
 	const prevLocationRef = useRef(location);
 	const tranLocationRef = useRef(location);
+	const transitioningRef = useRef(false);
+	const canRenderRef = useRef(false);
+	const timeout = useRef(null);
+
 	const switchMain = () => {
 		const newMain = mainRef.current === 'A' ? 'B' : 'A';
 		mainRef.current = newMain;
@@ -25,73 +30,62 @@ function AnimatedRouter() {
 		tranLocationRef.current = { ...prevLocationRef.current };
 		prevLocationRef.current = { ...location };
 		switchMain();
+		transitioningRef.current = true;
+		canRenderRef.current = true;
 	}
+
+	if (timeout.current) {
+		clearTimeout(timeout.current);
+		timeout.current = setTimeout(() => {
+			transitioningRef.current = false;
+			setRerenderTrigger((prev) => !prev);
+		}, 1000);
+	}
+
+	const transitioning = transitioningRef.current;
+	const canRenderB = canRenderRef.current;
 
 	const mainElement = useRoutes(Routes(), location);
 	const subElement = useRoutes(Routes(), tranLocationRef.current);
 
 	const isAMain = mainRef.current === 'A';
 	const isBMain = mainRef.current === 'B';
+
 	const elementA = isAMain ? mainElement : subElement;
 	const elementB = isBMain ? mainElement : subElement;
 
 	const showA = transitioning || isAMain;
 	const showB = transitioning || isBMain;
 
-	useEffect(() => {
-		let timer = null;
-		if (location.key !== prevLocation.key) {
-			setTransitioning(true);
-			// setTimeout(() => {
-			// 	debugger;
-			// }, 500);
-			timer = setTimeout(() => {
-				setTransitioning(false);
-				setPrevLocation(location);
-			}, 1000);
-		}
-		return () => {
-			if (timer) {
-				clearTimeout(timer);
-				setTransitioning(false);
-			}
-		};
-	}, [location, prevLocation, setPrevLocation]);
-
-	const customStyle = (isMain, show) => {
-		if (!isMain) {
-			return { display: show ? '' : 'none' };
-		} else {
-			return {
-				position: transitioning ? 'fixed' : 'relative',
-				display: show ? '' : 'none',
-				color: isMain ? 'red' : '',
-			};
-		}
-	};
+	const keyA = isAMain ? location.key : tranLocationRef.current.key;
+	const keyB = isBMain ? location.key : tranLocationRef.current.key;
 
 	return (
 		<div className="transition_wrap">
-			<div
-				className={
-					transitioning
-						? `${isAMain ? 'slideIn' : 'slideOut'}${transitionClass}`
-						: ''
-				}
-				style={customStyle(isAMain, showA)}
-			>
-				{elementA}
-			</div>
-			<div
-				className={
-					transitioning
-						? `${isBMain ? 'slideIn' : 'slideOut'}${transitionClass}`
-						: ''
-				}
-				style={customStyle(isBMain, showB)}
-			>
-				{elementB}
-			</div>
+			{showA && (
+				<div
+					className={
+						transitioning
+							? `${isAMain ? 'slidein' : 'slideout'} ${transitionClass}`
+							: ''
+					}
+					key={keyA}
+				>
+					{elementA}
+				</div>
+			)}
+			{showB && canRenderB && (
+				<div
+					className={
+						transitioning
+							? `${isBMain ? 'slidein' : 'slideout'} ${transitionClass}`
+							: ''
+					}
+					key={keyB}
+				>
+					{elementB}
+				</div>
+			)}
 		</div>
 	);
 }
